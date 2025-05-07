4c36109bd6331b0eedce04205e4eff8b0904f1d2
Tib <NotAn@ema.il>
2025-05-07 09:25:52 +0200

    TestAnyDataset added

--- examples/any/dataset.py
+++ examples/any/dataset.py
@@ -10,12 +10,14 @@ class TrainAnyDataset(PINNDataset):
     def __init__(self, u:Callable[[Tensor], Tensor], n_elements: int, n_colloc: int, shape: list[tuple[int, ...]], t_max: float = 1.0):
         super().__init__()
 
-        self.shape = shape
+        shape = torch.tensor(shape, dtype=torch.float32)
         self.u = u
 
         # shape = [(-1, 1), (0, 2)]
-        shape = torch.tensor(shape, dtype=torch.float32)
         
+        
+
+        # examples points
         elements: list[Tensor, Tensor] = [(
             a:=torch.concatenate([torch.zeros(1), torch.rand(len(shape))*(shape[:, 1] - shape[:, 0]) + shape[:, 0]]),
             self.u(a)
@@ -23,7 +25,7 @@ class TrainAnyDataset(PINNDataset):
         
 
         for dim in range(len(shape)):
-
+            # lower bound
             X= torch.rand(n_elements, len(shape))*(shape[:, 1] - shape[:, 0]) + shape[:, 0]
             X[:, dim] = torch.ones(n_elements)*shape[dim, 0]
 
@@ -32,6 +34,7 @@ class TrainAnyDataset(PINNDataset):
                 self.u(a)
             ) for x in X])
 
+            # upper bound
             X= torch.rand(n_elements, len(shape))*(shape[:, 1] - shape[:, 0]) + shape[:, 0]
             X[:, dim] = torch.ones(n_elements)*shape[dim, 1]
 
@@ -41,8 +44,25 @@ class TrainAnyDataset(PINNDataset):
             ) for x in X])
 
 
+        # collocation points
         colloc = [torch.concatenate([torch.rand(1)*t_max, torch.rand(len(shape))*(shape[:, 1] - shape[:, 0]) + shape[:, 0]])
                    for _ in range(n_colloc)]
 
 
+        self.set_elements(elements)
+        self.set_colloc(colloc)
 
+
+class TestAnyDataset(PINNDataset):
+    def __init__(self, u:Callable[[Tensor], Tensor], n_elements: int, shape: list[tuple[int, ...]], t_max: float = 1.0):
+        super().__init__()
+
+        shape = torch.tensor(shape, dtype=torch.float32)
+        self.u = u
+
+        elements: list[Tensor, Tensor] = [(
+            a:=torch.concatenate([torch.rand(1)*t_max, torch.rand(len(shape))*(shape[:, 1] - shape[:, 0]) + shape[:, 0]]),
+            self.u(a)
+        ) for _ in range(n_elements)]
+
+        self.set_elements(elements)
