06f848318756fee0988bb2491e60af7e606d180d
Tib <NotAn@ema.il>
2025-05-07 09:23:04 +0200

    TrainAnyDataset rewritten

--- examples/any/dataset.py
+++ examples/any/dataset.py
@@ -1,7 +1,6 @@
 import torch
 from torch import Tensor
 
-import numpy as np
 from typing import Callable
 
 from core.datasets.pinn_dataset import PINNDataset
@@ -12,58 +11,38 @@ class TrainAnyDataset(PINNDataset):
         super().__init__()
 
         self.shape = shape
-
         self.u = u
 
-        # JE DETESTE NUMPY !!!
-        x=np.random.uniform(*shape) if len(shape) > 1 else np.array([np.random.uniform(*shape[0])])
-        #print("x : ", x)
-        elements = [(
-            a:=torch.tensor(np.concatenate( (np.array([0]), x))),
-            torch.as_tensor(self.u(a), dtype=torch.float32) if len(shape) > 1 else torch.as_tensor([self.u(a)], dtype=torch.float32)
+        # shape = [(-1, 1), (0, 2)]
+        shape = torch.tensor(shape, dtype=torch.float32)
+        
+        elements: list[Tensor, Tensor] = [(
+            a:=torch.concatenate([torch.zeros(1), torch.rand(len(shape))*(shape[:, 1] - shape[:, 0]) + shape[:, 0]]),
+            self.u(a)
         ) for _ in range(n_elements)]
+        
 
         for dim in range(len(shape)):
-            x=np.random.uniform(*shape) if len(shape) > 1 else np.array([np.random.uniform(*shape[0])])
-            x[dim] = shape[dim][0]
+
+            X= torch.rand(n_elements, len(shape))*(shape[:, 1] - shape[:, 0]) + shape[:, 0]
+            X[:, dim] = torch.ones(n_elements)*shape[dim, 0]
+
             elements.extend([(
-                a:=torch.tensor(np.concatenate(( np.array([np.random.uniform(0, t_max)]),  x )) ),
-                torch.as_tensor(self.u(a), dtype=torch.float32) if len(shape) > 1 else torch.as_tensor([self.u(a)], dtype=torch.float32)
-            ) for _ in range(n_elements)])
+                a:= torch.concatenate([torch.rand(1)*t_max, x]),
+                self.u(a)
+            ) for x in X])
 
-            x=np.random.uniform(*shape) if len(shape) > 1 else np.array([np.random.uniform(*shape[0])])
-            x[dim] = shape[dim][1]
+            X= torch.rand(n_elements, len(shape))*(shape[:, 1] - shape[:, 0]) + shape[:, 0]
+            X[:, dim] = torch.ones(n_elements)*shape[dim, 1]
+
             elements.extend([(
-                a:=torch.tensor(np.concatenate(( np.array([np.random.uniform(0, t_max)]),  x )) ),
-                torch.as_tensor(self.u(a), dtype=torch.float32) if len(shape) > 1 else torch.as_tensor([self.u(a)], dtype=torch.float32)
-            ) for _ in range(n_elements)])
+                a:= torch.concatenate([torch.rand(1)*t_max, x]),
+                self.u(a)
+            ) for x in X])
 
 
-        colloc = [torch.tensor(np.concatenate((np.array([np.random.uniform(0, t_max)]), np.random.uniform(*shape) if len(shape) > 1 else np.array([np.random.uniform(*shape[0])]) ))) for _ in range(n_colloc)]
-
-        self.set_elements(elements)
-        self.set_colloc(colloc)
+        colloc = [torch.concatenate([torch.rand(1)*t_max, torch.rand(len(shape))*(shape[:, 1] - shape[:, 0]) + shape[:, 0]])
+                   for _ in range(n_colloc)]
 
 
-class TestAnyDataset(PINNDataset):
-    def __init__(self, u:Callable[[Tensor], Tensor], n_elements: int, shape: list[tuple[int, ...]], t_max: float = 1.0):
-        super().__init__()
 
-        self.shape = shape
-
-        self.u = u
-
-        x=np.random.uniform(*shape) if len(shape) > 1 else np.array([np.random.uniform(*shape[0])])
-        elements = [(
-            a:=torch.tensor(np.concatenate(( np.array([np.random.uniform(0, t_max)]),  x )) ),
-            torch.as_tensor(self.u(a), dtype=torch.float32) if len(shape) > 1 else torch.as_tensor([self.u(a)], dtype=torch.float32)
-        ) for _ in range(n_elements)]
-
-        self.set_elements(elements)
-
-    
-            
-        
-            
-            
-        
