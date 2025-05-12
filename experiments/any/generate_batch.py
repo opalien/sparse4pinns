f6fd62d49f3b15f1ba4f4eb8454644b01e2b5449
Tib <NotAn@ema.il>
2025-05-12 07:40:47 +0200

    params generation

--- experiments/any/generate_batch.py
+++ experiments/any/generate_batch.py
@@ -10,10 +10,8 @@ rep = 10
 if os.path.exists("params"):
     os.remove("params")
 
-for (m, k, model, equation) in [(m, k) for m in M for k in K for model in models for equation in equations]:
-    with open(f"params", "a") as f:
+for (m, k, model, equation) in [(m, k, model, equation) for m in M for k in K for model in models for equation in equations]:
+    with open(f"experiments/any/params", "a") as f:
         for r in range(rep):
-
-            #f.write(f"simple -m {m} -k {k} -e {epoch} -r 1\n")
             f.write(f"{equation} -m {m} -k {k} -e {epoch} -r {r} -f {model}\n")
 
