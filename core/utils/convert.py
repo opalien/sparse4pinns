743e35653d7e3c481d34ed9aa01dd0b8ae54c984
Tib <NotAn@ema.il>
2025-05-17 19:03:42 +0200

    monarch_to_linear & steam_to_linear added

--- core/utils/convert.py
+++ core/utils/convert.py
@@ -37,4 +37,38 @@ def linear_to_steam(model: PINN, T=100, alpha=0.001):
 
     model.layers = nn.ModuleList(layers)
     return model
-                
+
+
+def monarch_to_linear(model: PINN):
+    layers: list[nn.Module] = []
+    for layer in model.layers:
+        if isinstance(layer, MonarchLinear):
+            linear_layer = nn.Linear(layer.in_features, layer.out_features, bias=True)
+
+            linear_layer.weight.data = layer.tensor().dense.clone()
+            linear_layer.bias.data = layer.bias.clone()
+            layers.append(linear_layer)
+        else:
+            layers.append(layer)
+
+    model.layers = nn.ModuleList(layers)
+    return model
+
+
+def steam_to_linear(model: PINN):
+    layers: list[nn.Module] = []
+    for layer in model.layers:
+        if isinstance(layer, STEAMLinear):
+            # Create a new nn.Linear layer
+            linear_layer = nn.Linear(layer.in_features, layer.out_features, bias=True)
+            
+            # Copy the weights from the STEAMLinear layer's dense representation
+            linear_layer.weight.data = layer.dense.clone()
+            linear_layer.bias.data = layer.bias.clone()
+
+            layers.append(linear_layer)
+        else:
+            layers.append(layer)
+    
+    model.layers = nn.ModuleList(layers)
+    return model
